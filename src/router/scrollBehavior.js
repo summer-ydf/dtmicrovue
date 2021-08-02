c97dde5a73442ff4e89070bcc3fba5761a6d90fb
sakuya <81883387@qq.com>
2021-08-02 18:56:41 +0800

    Update scrollBehavior.js

--- src/router/scrollBehavior.js
+++ src/router/scrollBehavior.js
@@ -15,6 +15,8 @@ export function afterEach(to){
 	if(!adminMain){return false}
 	nextTick(()=>{
 		var beforeRoute = store.state.viewTags.viewTags.filter(v => v.fullPath == to.fullPath)[0]
-		adminMain.scrollTop = beforeRoute.scrollTop || 0
+		if(beforeRoute){
+			adminMain.scrollTop = beforeRoute.scrollTop || 0
+		}
 	})
 }
