8ff3f6b0ffe578c87373ae23a6a2e3240b42baf3
sc <shencheng@heronshn.com>
2021-11-04 16:13:38 +0800

    UPD scTable 增加自定义查询插糟

--- src/views/vab/tableselect.vue
+++ src/views/vab/tableselect.vue
@@ -18,9 +18,28 @@
 		</el-card>
 		<div style="height:15px"></div>
 		<el-card shadow="never" header="多选">
-			<sc-table-select v-model="value" :apiObj="apiObj" multiple :props="props">
+			<sc-table-select v-model="value" :apiObj="apiObj" :table-width="700" multiple :props="props">
+				<template #header="{form, submit}">
+					<el-form :inline="true" :model="form">
+						<el-form-item>
+							<el-select v-model="form.sex" placeholder="性别" clearable :popper-append-to-body="false">
+								<el-option label="男" value="1"></el-option>
+								<el-option label="女" value="2"></el-option>
+							</el-select>
+						</el-form-item>
+						<el-form-item>
+							<el-date-picker v-model="form.date" value-format="YYYY-MM-DD" type="date" placeholder="注册时间" :append-to-body="false"></el-date-picker>
+						</el-form-item>
+						<el-form-item>
+							<el-button type="primary" @click="submit">查询</el-button>
+						</el-form-item>
+					</el-form>
+					<el-alert title="自定义FORM插糟 传递了form对象和提交方法" type="info" />
+				</template>
 				<el-table-column prop="id" label="ID" width="150"></el-table-column>
-				<el-table-column prop="user" label="姓名"></el-table-column>
+				<el-table-column prop="user" label="姓名" width="100"></el-table-column>
+				<el-table-column prop="cip" label="最后请求IP" width="150"></el-table-column>
+				<el-table-column prop="time" label="注册时间"></el-table-column>
 			</sc-table-select>
 		</el-card>
 	</el-main>
