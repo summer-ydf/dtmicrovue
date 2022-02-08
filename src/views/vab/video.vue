48f047ba7dc6715c9d0410144e55a069cb4fdcf2
sc <shencheng@heronshn.com>
2022-02-08 11:10:43 +0800

    Update video.vue

--- src/views/vab/video.vue
+++ src/views/vab/video.vue
@@ -34,7 +34,7 @@
 			<el-col :lg="12">
 				<el-card shadow="never" header="HlS(m3u8)直播">
 					<sc-video
-						src="http://cctvalih5ca.v.myalicdn.com/live/cctv5_2/index.m3u8"
+						src="http://hsplay-360.v.btime.com/live_btime/btv_sn_20170706_s4/index.m3u8"
 						isLive
 					></sc-video>
 				</el-card>
