c1758ed352d91b4bc406cab9f283ffc1c151497e
yandongfa <1973984292@qq.com>
2022-02-28 14:32:09 +0800

    '修改抽屉样式'

--- src/views/setting/user/update.vue
+++ src/views/setting/user/update.vue
@@ -1,32 +1,39 @@
 <template>
 		<el-drawer :title="titleMap[mode]" v-model="visible" :size="400" append-to-body destroy-on-close @closed="$emit('closed')">
-			<el-form :model="form" :rules="rules" :disabled="mode==='show'" ref="dialogForm"
-					 label-width="120px" label-position="left" style="padding:0 20px;">
-				<el-form-item label="登录账号" prop="username">
-					<el-input v-model="form.username" placeholder="用于登录系统" clearable></el-input>
-				</el-form-item>
-				<el-form-item label="姓名" prop="name">
-					<el-input v-model="form.name" placeholder="请输入完整的真实姓名" clearable></el-input>
-				</el-form-item>
-				<template v-if="mode==='add'">
-					<el-form-item label="登录密码" prop="password">
-						<el-input type="password" v-model="form.password" clearable show-password></el-input>
-					</el-form-item>
-					<el-form-item label="确认密码" prop="password2">
-						<el-input type="password" v-model="form.password2" clearable show-password></el-input>
-					</el-form-item>
-				</template>
-<!--				<el-form-item label="所属角色" prop="group">-->
-<!--					<el-cascader v-model="form.group" :options="groups" :props="groupsProps" :show-all-levels="false" clearable style="width: 100%;"></el-cascader>-->
-<!--				</el-form-item>-->
-				<!--				<el-form-item label="头像" prop="avatar">-->
-				<!--					<sc-upload v-model="form.avatar" title="上传头像"></sc-upload>-->
-				<!--				</el-form-item>-->
-				<template>
-					<el-button @click="visible=false" >取 消</el-button>
-					<el-button v-if="mode!=='show'" type="primary" :loading="isSaveing" @click="submit()">保 存</el-button>
-				</template>
-			</el-form>
+			<el-row class="drawer-table">
+				<el-col :span="24">
+					<el-form :model="form" :rules="rules" :disabled="mode==='show'" ref="dialogForm" label-width="80px">
+						<el-form-item label="登录账号" prop="username">
+							<el-input v-model="form.username" placeholder="请输入登录账号" clearable></el-input>
+						</el-form-item>
+						<template v-if="mode==='add'">
+							<el-form-item label="登录密码" prop="password">
+								<el-input type="password" v-model="form.password" placeholder="请输入登录密码" clearable show-password></el-input>
+							</el-form-item>
+							<el-form-item label="确认密码" prop="password2">
+								<el-input type="password" v-model="form.password2" placeholder="请确认密码" clearable show-password></el-input>
+							</el-form-item>
+						</template>
+						<!--				<el-form-item label="所属角色" prop="group">-->
+						<!--					<el-cascader v-model="form.group" :options="groups" :props="groupsProps" :show-all-levels="false" clearable style="width: 100%;"></el-cascader>-->
+						<!--				</el-form-item>-->
+						<!--				<el-form-item label="头像" prop="avatar">-->
+						<!--					<sc-upload v-model="form.avatar" title="上传头像"></sc-upload>-->
+						<!--				</el-form-item>-->
+<!--						<template>-->
+<!--							<el-button @click="visible=false" >取 消</el-button>-->
+<!--							<el-button v-if="mode!=='show'" type="primary" :loading="isSaveing" @click="submit()">保 存</el-button>-->
+<!--						</template>-->
+					</el-form>
+				</el-col>
+				<el-row class="drawer-footer">
+					<el-col :span="24">
+						<el-button @click="visible=false" size="small">取 消</el-button>
+						<el-button type="primary" size="small">保 存</el-button>
+					</el-col>
+				</el-row>
+			</el-row>
+
 
 		</el-drawer>
 </template>
